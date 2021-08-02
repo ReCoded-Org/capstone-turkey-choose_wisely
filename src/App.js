88da8392c5c241cca20ff446b0af5e980c0e20a6
Ammar-64 <ammar.n.sammour@gmail.com>
2021-08-02 19:28:20 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>lsjnfiowjp ksjvklw</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
