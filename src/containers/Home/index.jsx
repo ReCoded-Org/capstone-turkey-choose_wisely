9729bbf2a69fcb1438ad9b5b1850c2e8e34efbe2
DKay-code <derya.kaygisiz@outlook.com>
2021-08-19 22:03:00 +0300

    passed the tests

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
 import WhoAreWe from "./../../components/WhoAreWe";
+import Map from "./../../components/Map";
 
 const Home = () => {
   return (
