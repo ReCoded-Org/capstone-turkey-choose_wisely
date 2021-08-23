f9b9df683137de52b5cae4254d717d10b42a72a3
DKay-code <derya.kaygisiz@outlook.com>
2021-08-23 16:33:25 +0300

    RessourcesPage Header Section --> Done

--- src/test/RessourcesHeader.test.js
+++ src/test/RessourcesHeader.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import RessourcesHeader from "./../components/RessourcesHeader/index.jsx";
 import renderer from "react-test-renderer";
 
-
 test("Snapshop test of RessourcesHeader component", () => {
   const map = renderer.create(<RessourcesHeader />);
   let tree = map.toJSON();
