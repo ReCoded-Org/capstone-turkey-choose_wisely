865415e950d716cee95e9dd0c178088d69b6187c
khadijahawa <khadijahawa555@gmail.com>
2021-08-24 16:28:39 +0300

    fix the tests

--- src/components/Cards/Index.jsx
+++ src/components/Cards/Index.jsx
@@ -14,6 +14,26 @@ export default function WhatDidTheyAboutUsCard() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
   const { t } = useTranslation();
 
