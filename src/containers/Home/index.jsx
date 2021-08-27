c52c65fd16fb28d517dbaad453c8f13445d61c4b
MohammedZakaria2 <mohammed.alalaya@gmail.com>
2021-08-27 10:27:30 +0300

    a temp link have been addded

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./style.scss";
 import WhoAreWe from "./../../components/WhoAreWe";
 import Map from "./../../components/Map";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <main className="home">
+      <Link to="/universities">Go To Universities Page</Link>
+
       <WhoAreWe />
       <Map />
     </main>
