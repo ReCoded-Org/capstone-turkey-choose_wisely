086d4752ea17af5194b7075b47a1c74d907b6910
abdulrahman <abdulrahmantaji99@gmail.com>
2021-08-28 21:01:05 +0300

    delete the .yarn file and edit slider images and fix some responsive issues

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path="/" component={Contact} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/ressources" component={RessourcesPage} />
       </Switch>
       <Footer />
     </Router>
