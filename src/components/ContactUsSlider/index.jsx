d37137b90a855df2217c691129e5dd4768f59a13
abdulrahman <abdulrahmantaji99@gmail.com>
2021-08-29 21:15:54 +0300

    done .

--- src/components/ContactUsSlider/index.jsx
+++ src/components/ContactUsSlider/index.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
 import "./style.scss";
-// import { Col, Container, Row, Image, Button  } from "react-bootstrap";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { FaBeer } from "react-icons/fa";
-import { BsChatDotsFill } from "react-icons/bs";
 
-import { FiPhoneForwarded } from "react-icons/fi";
 import EmailsReCqen from "./../../images/ContactPageimgs/undraw_Emails_re_cqen.svg";
 import PublicDiscussion from "./../../images/ContactPageimgs/undraw_Public_discussion_re_w9up.svg";
 
 import WallPostRe from "./../../images/ContactPageimgs/undraw_Wall_post_re_y78d.svg";
 
-// FiPhoneForwarded
-// FiPhoneForwarded
-//BsChatDotsFill
-// FiPhoneForwarded
-// "./../../images/icons/Pinterest.svg"
-// /static/media/undraw_Chatting_re_j55r 1.b4f78395.svg
+
 
 const photos = [
   {
