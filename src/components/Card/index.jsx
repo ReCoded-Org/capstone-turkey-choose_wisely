8be45de6984e240b52a72353561fc870c2bf257c
khadijahawa <khadijahawa555@gmail.com>
2021-08-30 13:28:43 +0300

    reviews modified

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -7,16 +7,18 @@ export default function SliderSecion(speech) {
   console.log(speech);
 
   return (
+    
     <Container>
+      
       <Row className="align-items-center justify-content-center Card">
         <Col className="ps-5 avatarComponent" md={2}>
           <Avatar className="Avatar" alt="Remy Sharp" />
         </Col>
 
         <Col className="col-md-offset-2 SpeechComponent" md={10}>
-          <p className="paragraph">{speech.part1}</p>
-          <h5 className="auther">{speech.part2}</h5>
-          <h6 className="description">{speech.part3}</h6>
+          <p className="paragraph">{speech.speechText}</p>
+          <h5 className="auther">{speech.auther}</h5>
+          <h6 className="description">{speech.AutherSection}</h6>
         </Col>
       </Row>
     </Container>
--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,12 +1,34 @@
+import React from "react";
 import SliderSecion from "../Card";
 import { Container } from "react-bootstrap";
 import "./style.scss";
-import React from "react";
 import { useTranslation } from "react-i18next";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
+// function SampleNextArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: "block", background :"#386641" }}
+//       onClick={onClick}
+//     />
+//   );
+// }
+
+// function SamplePrevArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: "flex", background: "#FFFFFF "  }}
+//       onClick={onClick}
+//     />
+//   );
+// }
 export default function WhatDidTheyAboutUsCard() {
   const settings = {
     dots: true,
@@ -14,13 +36,15 @@ export default function WhatDidTheyAboutUsCard() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    // nextArrow: <SampleNextArrow />,
+    // prevArrow: <SamplePrevArrow />,
     responsive: [
       {
         breakpoint: 768,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: "40px",
+          centerPadding: "2.5rem",
           slidesToShow: 3,
         },
       },
@@ -36,11 +60,11 @@ export default function WhatDidTheyAboutUsCard() {
     ],
   };
   const { t } = useTranslation();
-
+  
   return (
     <div className="card">
       <Container>
-        <h2 className="header">What did they say about us</h2>
+        <h2 className="header">{t('home.WhatDidTheySayAboutUs.HeaderSection')}</h2>
 
         <Slider {...settings}>
           {t("home.speeches", { returnObjects: true }).map((speech) => (
