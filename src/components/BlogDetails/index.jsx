e9f42912f6bb2c5127e2a67dfae6bd70500e759f
khadijahawa <khadijahawa555@gmail.com>
2021-08-31 15:46:15 +0300

    finished blog page

--- src/components/BlogDetails/index.jsx
+++ src/components/BlogDetails/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "./style.scss";
 import Avatar from "@material-ui/core/Avatar";
 
