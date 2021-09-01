081b314ab72e8a620a6dedfe960f73b5f2b5bfee
MohammedZakaria2 <mohammed.alalaya@gmail.com>
2021-09-01 14:43:53 +0300

    conflec solved

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import history from "./history";
 
 import "./App.scss";
 import "./style/style.css";
-import RessourcesPage from "./containers/RessourcesPage";
+// import RessourcesPage from "./containers/RessourcesPage";
 
 function App() {
   return (
@@ -18,12 +18,8 @@ function App() {
       <Preloader />
       <Switch>
         <Route exact path="/" component={Home} />
-<<<<<<< HEAD
         <Route exact path="/universities" component={Universities} />
         <Route component={NotFound} />
-=======
-        <Route exact path="/ressources" component={RessourcesPage} />
->>>>>>> f12fbb44da3a3b54fc9d9f33b657240279b8c5e7
       </Switch>
       <Footer />
     </Router>
